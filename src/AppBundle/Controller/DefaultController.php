8b3ba40098e5ee00bd67cde95c102120680cbcfd
KrunoKnego <k.knego@gmail.com>
2015-07-30 22:20:58 +0200

    Added home

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,9 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        if($this->getUser() instanceof UserInterface) {
+            return $this->render('default/index.html.twig');
+        }
         return $this->render('default/index.html.twig');
     }
 }
