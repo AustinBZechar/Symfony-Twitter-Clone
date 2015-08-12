8f30f6b23508ee30191abe72039f327e48e89051
KrunoKnego <k.knego@gmail.com>
2015-08-12 22:41:17 +0200

    Added login transfer object.

---
+++ src/AppBundle/Transfer/LoginTransfer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Transfer;
+
+class LoginTransfer
+{
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
