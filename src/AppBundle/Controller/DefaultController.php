c78ee369116e299c691685a227c62a0f3bf346a6
KrunoKnego <k.knego@gmail.com>
2015-08-12 22:50:17 +0200

    Added login functionality to the defaultController.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Form\RegistrationType;
+use AppBundle\Transfer\LoginTransfer;
 use AppBundle\Transfer\RegistrationTransfer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -29,22 +30,25 @@ class DefaultController extends Controller
         $registrationTransferObject = new RegistrationTransfer();
         $registrationForm = $this->initForm($request, $registrationTransferObject);
 
+        $loginTransferObject = new LoginTransfer();
+        $loginForm = $this->initForm($request, $loginTransferObject);
+
         if ($registrationForm->isSubmitted() && $registrationForm->isValid()) {
             $authSecret = $this->get('app.redis.redis_registration')->register(
                 $registrationTransferObject->getUsername(),
                 $registrationTransferObject->getPassword()
             );
 
-            $cookie = new Cookie(
-                'auth',
-                $authSecret,
-                new \DateTime(self::HOUR_IN_SECONDS * self::DAY_IN_HOURS * self::YEAR_IN_DAYS)
+            return $this->generateCookieResponse($authSecret);
+        }
+
+        if ($loginForm->isSubmitted() && $loginForm->isValid()) {
+            $authSecret = $this->get('app.redis.redis_registration')->register(
+                $loginTransferObject->getUsername(),
+                $loginTransferObject->getPassword()
             );
 
-            $response = new Response($this->renderView(':default:home.html.twig'));
-            $response->headers->setCookie($cookie);
-
-            return $response;
+            return $this->generateCookieResponse($authSecret);
         }
 
         return $this->render(':default:index.html.twig', [
@@ -63,5 +67,23 @@ class DefaultController extends Controller
         $form = $this->createForm(new RegistrationType(), $transferObject);
         $form->handleRequest($request);
         return $form;
+    }
+
+    /**
+     * @param $authSecret
+     * @return Response
+     */
+    private function generateCookieResponse($authSecret)
+    {
+        $cookie = new Cookie(
+            'auth',
+            $authSecret,
+            new \DateTime(self::HOUR_IN_SECONDS * self::DAY_IN_HOURS * self::YEAR_IN_DAYS)
+        );
+
+        $response = new Response($this->renderView(':default:home.html.twig'));
+        $response->headers->setCookie($cookie);
+
+        return $response;
     }
 }
