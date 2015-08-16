dfcd915fe2a5f472bbfc53a4e1af4e1828d3aecd
KrunoKnego <k.knego@gmail.com>
2015-08-16 16:28:53 +0200

    Added profile & follow/unfollow functionality

---
+++ src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProfileController extends Controller
+{
+    /**
+     * @param Request $request
+     * @param         $userId
+     *
+     * @return Response
+     * @Route("/profile/{userId}", name="profile")
+     */
+    public function profileAction(Request $request, $userId)
+    {
+        return $this->render(':default:profile.html.twig', [
+            'tweets' => $this->get('app.redis.redis_tweet')->showUserPosts(),
+            'userId' => $userId,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param         $userId
+     *
+     * @return Response
+     * @Route("/follow/{userId}", name="follow")
+     */
+    public function followAction(Request $request, $userId)
+    {
+        $this->get('app.redis.redis_follow')->followOrUnfollow($userId);
+
+        return $this->render(':default:profile.html.twig', [
+            'tweets' => $this->get('app.redis.redis_tweet')->showUserPosts(),
+            'userId' => $userId,
+        ]);
+    }
+}
---
+++ src/AppBundle/Redis/RedisFollow.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Redis;
+
+use Predis\Client;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class RedisFollow
+{
+    /**
+     * @var Client
+     */
+    private $redisClient;
+
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @param Client $redisClient
+     * @param Session $session
+     */
+    public function __construct(Client $redisClient, Session $session)
+    {
+        $this->redisClient = $redisClient;
+        $this->session = $session;
+    }
+
+    /**
+     * @param integer $userId
+     */
+    private function follow($userId)
+    {
+        $sessionUserId = $this->session->get('userId');
+        if ($userId != $sessionUserId) {
+            $this->redisClient->sadd("uid:".$userId.":followers", [
+                $sessionUserId,
+            ]);
+            $this->redisClient->sadd("uid:".$sessionUserId.":following", [
+                $userId,
+            ]);
+        }
+    }
+
+    /**
+     * @param integer $userId
+     */
+    private function unfollow($userId)
+    {
+        $sessionUserId = $this->session->get('userId');
+        if ($userId != $sessionUserId) {
+            $this->redisClient->srem("uid:".$userId.":followers", [
+                $sessionUserId,
+            ]);
+            $this->redisClient->srem("uid:".$sessionUserId.":following", [
+                $userId,
+            ]);
+        }
+    }
+
+    /**
+     * @param integer $userId
+     */
+    public function followOrUnfollow($userId)
+    {
+        $sessionUserId = $this->session->get('userId');
+        if ($this->redisClient->sismember("uid:".$sessionUserId.":following", $userId) === 1) {
+            $this->follow($userId);
+        }
+
+        $this->unfollow($userId);
+    }
+}
